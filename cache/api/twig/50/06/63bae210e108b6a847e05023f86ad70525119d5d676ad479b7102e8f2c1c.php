c86da6f9cb7f4d282b2e2b74513faedcf0a4e9fc
Andy Miller <rhuk@mac.com>
2015-12-08 20:02:07 -0700

    Fix for crazy toolbox paths

--- cache/api/twig/50/06/63bae210e108b6a847e05023f86ad70525119d5d676ad479b7102e8f2c1c.php
+++ cache/api/twig/50/06/63bae210e108b6a847e05023f86ad70525119d5d676ad479b7102e8f2c1c.php
@@ -37,7 +37,7 @@ class __TwigTemplate_500663bae210e108b6a847e05023f86ad70525119d5d676ad479b7102e8
     {
         // line 5
         echo "\t";
-        if ((is_string($__internal_62fad6cf05502351533cedf83d51ee9f90e5a786a48229d5d137434deef53c25 = $this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "name", array())) && is_string($__internal_2ada05e6513e6840288c2691864f26cbab6ba347bbfbdf88a7c1445ad048d52e = "Grav") && ('' === $__internal_2ada05e6513e6840288c2691864f26cbab6ba347bbfbdf88a7c1445ad048d52e || 0 === strpos($__internal_62fad6cf05502351533cedf83d51ee9f90e5a786a48229d5d137434deef53c25, $__internal_2ada05e6513e6840288c2691864f26cbab6ba347bbfbdf88a7c1445ad048d52e)))) {
+        if ((is_string($__internal_4e418fe9817772f5e0e372d69362f3f51fb73ad343e528c8e6181f4396c02baa = $this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "name", array())) && is_string($__internal_2ff5d8c427c203393f9f12e35441ef918b82354ef3e2b4b35e997d83dcb6e561 = "Grav") && ('' === $__internal_2ff5d8c427c203393f9f12e35441ef918b82354ef3e2b4b35e997d83dcb6e561 || 0 === strpos($__internal_4e418fe9817772f5e0e372d69362f3f51fb73ad343e528c8e6181f4396c02baa, $__internal_2ff5d8c427c203393f9f12e35441ef918b82354ef3e2b4b35e997d83dcb6e561)))) {
             // line 6
             echo "\t\t";
             $context["github_link"] = (("https://github.com/getgrav/grav/blob/develop/system/src/" . strtr($this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "name", array()), array("\\" => "/"))) . ".php");
@@ -46,16 +46,19 @@ class __TwigTemplate_500663bae210e108b6a847e05023f86ad70525119d5d676ad479b7102e8
         } else {
             // line 8
             echo "\t\t";
-            $context["github_link"] = (("https://github.com/" . strtr($this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "name", array()), array($this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "shortname", array()) => ("src/" . $this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "shortname", array())), "RocketTheme" => "rockettheme", "Toolbox" => "toolbox/blob/develop", "\\" => "/"))) . ".php");
+            $context["github_link"] = (("https://github.com/" . strtr($this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "name", array()), array("RocketTheme" => "rockettheme", "Toolbox" => "toolbox/blob/develop", "\\" => "/"))) . ".php");
             // line 9
+            echo "\t\t";
+            $context["github_link"] = strtr((isset($context["github_link"]) ? $context["github_link"] : $this->getContext($context, "github_link")), array(($this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "shortname", array()) . ".php") => (("src/" . $this->getAttribute((isset($context["class"]) ? $context["class"] : $this->getContext($context, "class")), "shortname", array())) . ".php")));
+            // line 10
             echo "\t";
         }
-        // line 10
+        // line 11
         echo "\t<a class=\"btn btn-warning pull-right\" href=\"";
         echo twig_escape_filter($this->env, (isset($context["github_link"]) ? $context["github_link"] : $this->getContext($context, "github_link")), "html", null, true);
         echo "\">Edit on GitHub</a>
 \t";
-        // line 11
+        // line 12
         $this->displayParentBlock("page_content", $context, $blocks);
         echo "
 ";
@@ -73,6 +76,6 @@ class __TwigTemplate_500663bae210e108b6a847e05023f86ad70525119d5d676ad479b7102e8
 
     public function getDebugInfo()
     {
-        return array (  59 => 11,  54 => 10,  51 => 9,  48 => 8,  45 => 7,  42 => 6,  39 => 5,  36 => 4,  11 => 1,);
+        return array (  62 => 12,  57 => 11,  54 => 10,  51 => 9,  48 => 8,  45 => 7,  42 => 6,  39 => 5,  36 => 4,  11 => 1,);
     }
 }
